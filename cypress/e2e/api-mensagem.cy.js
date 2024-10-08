91142af2afb2d4913f63157572c6c812e8cc7e97
kamilla-Carvalho <70145643+kamilla-Carvalho@users.noreply.github.com>
2024-10-08 11:51:43 -0300

    Finalização do projeto

--- cypress/e2e/api-mensagem.cy.js
+++ cypress/e2e/api-mensagem.cy.js
@@ -1,11 +1,10 @@
 describe('Api Adopet', ()=> {
-    const authorization = `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI4MTFkMjdlNC0xYTdiLTRkZWItYTFiYS0wNjRjYTgxNzM2ZDMiLCJhZG9wdGVyTmFtZSI6IkthbWlsbGEgQ2FydmFsaG8iLCJpYXQiOjE3MjgwNjQyNDYsImV4cCI6MTcyODMyMzQ0Nn0.N4q6h_DIiSVEj1FWnTXEpQQ3BT-n5WZ7OqDpAG6fLh4`
 
     it('Mensagens da API', () => {
         cy.request({
             method: 'GET', 
             url:'https://adopet-api-i8qu.onrender.com/mensagem/811d27e4-1a7b-4deb-a1ba-064ca81736d3',
-            headers:{ authorization }
+            headers: Cypress.env()
         }).then((res) => {
             expect(res.status).to.be.equal(200)
             expect(res.body).is.not.empty
--- cypress/e2e/perfil-usuario.cy.js
+++ cypress/e2e/perfil-usuario.cy.js
@@ -4,26 +4,18 @@ describe('Página Perfil', ()=> {
         cy.get('[data-test="login-button"]').click();
         cy.login('kprado@websupply.com.br', 'Acesso123')
     })
-    it('Deve exibir o nome do usuário ao acessar a página de perfil', () => {
-        cy.intercept({
-            method: 'GET',
-            url: 'https://adopet-api-i8qu.onrender.com/adotante/perfil',
-            headers: {
-              Authorization: `Bearer 811d27e4-1a7b-4deb-a1ba-064ca81736d3` // Trocar pelo token real se necessário
-            }
-          }, (req) => {
-            req.reply({
-              statusCode: 200,
-              body: {
-                nome: 'Kprado',  // Simulação de nome retornado pela API
-                email: 'kprado@websupply.com.br'
-              }
-            });
-          }).as('getPerfil');
-        cy.get('.header__user').click();
-        cy.get('.button').click();
-        cy.wait('@getPerfil');
-        cy.get('#nome').should('be.visible').and('contain','Kamilla Carvalho');
-        //cy.contains('').should('be.visible')
-    })    
-})
+  
+          it('Nome do perfil', () => {
+              cy.request({
+                  method: 'GET' ,
+                  url: 'https://adopet-api-i8qu.onrender.com/adotante/perfil/811d27e4-1a7b-4deb-a1ba-064ca81736d3'
+                 
+              }).then((res) => {
+                  expect(res.status).to.be.equal(200)
+                  expect(res.body).is.not.empty
+                  expect(res.body).to.have.property('perfil')
+              expect(res.body.perfil.nome).to.be.equal('Kamilla Carvalho')
+                               
+              })
+          })
+      })
